69da501180b2a13e3fc26da9d3364e26bc50a540
Matheus <matheus.barreto@eaportal.org>
2023-09-22 09:47:02 -0300

    recapitulação de node com express

---
+++ 2_EXPRESS/3_render_html/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const path=require('path')
+const app = express()
+
+const caminho = path.join(__dirname, 'templates')
+
+app.get('/', (requisicao,resposta)=> {
+resposta.sendFile(`${caminho}/index.html`)
+} )
+
+app.listen(3000, () => {
+    console.log("servidor mudando na porta 3000")
+})
